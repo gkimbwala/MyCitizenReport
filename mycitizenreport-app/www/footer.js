21bf01f2f595ee54831e3732d2807548f1121526
Jim Bridgewater <jwbwater@gmail.com>
2015-05-17 10:00:31 -0500

    Sticky CSS footer.
    Created css/stickyfooter.css and added it to the html files.

--- mycitizenreport-app/www/footer.js
+++ mycitizenreport-app/www/footer.js
@@ -1,12 +1,13 @@
 
 document.write('\
-<footer>\
+<div id="footer">\
     <center>\
         <a href="registration.html">Registration</a>\
 	    <a href="startincident.html">Incident</a>\
 	    <a href="links.html">Tips</a>\
 	    <a href="kudos.html">Kudos</a>\
+	    <a href="complaints.html">Complaints</a>\
     <center/>\
-</footer>\
+</div>\
 ');
 
