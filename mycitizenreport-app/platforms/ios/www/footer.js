ac71789725aada46e692aaece04e0dc3864293f1
gkimbwala <gkimbwala@gmail.com>
2015-05-17 05:03:32 -0500

    added footer links
    added the links to footer script to the footer of index.

---
+++ mycitizenreport-app/platforms/ios/www/footer.js
@@ -0,0 +1,13 @@
+
+document.write('\
+<footer>\
+    <center>\
+        <a href="registration.html">Registration</a>\
+	    <a href="startincident.html">Start Incident</a>\
+	    <a href="links.html">Helpful Links</a>\
+	    <a href="Complaints">Complaints</a>\
+	    <a href="kudos.html">Kudos</a>\
+    <center/>\
+</footer>\
+');
+
